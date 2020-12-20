8575d3e641513d363edc6f110636169ba5c952ce
Demjan <demjan.vester@gmail.com>
2020-12-20 19:13:07 +0100

    format

--- screenshots.js
+++ screenshots.js
@@ -4,13 +4,13 @@ module.exports = {
     waitForSelector: ".card-number",
     pageWidth: 560,
     pageHeight: 670,
-    scrollY: 70
+    scrollY: 70,
   },
   homeTop3Cards: {
     url: "https://covid-19.sledilnik.org/",
     waitForSelector: ".card-number",
     pageWidth: 992,
     pageHeight: 275,
-    scrollY: 70
+    scrollY: 70,
   },
 };
