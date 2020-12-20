071efdcb35e02cfbac51d9047929a2691cca1e57
Demjan <demjan.vester@gmail.com>
2020-12-20 12:57:07 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const chromium = require('chrome-aws-lambda');
+
+export const handler = async (event, context, callback) => {
+  let result = 'result';
+  let browser;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+
+    // all your puppeteer things
+  } catch (error) {
+    return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return callback(null, result);
+};
