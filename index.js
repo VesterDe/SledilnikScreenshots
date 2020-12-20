6f170e9499c27f35a15001cf5fa1f0393e63b666
Demjan <demjan.vester@gmail.com>
2020-12-20 22:38:46 +0100

    filename test

--- index.js
+++ index.js
@@ -24,40 +24,46 @@ const handler = async (event, context, callback) => {
       ignoreHTTPSErrors: true,
     });
     console.log("Made browser");
+
     const page = await browser.newPage();
     console.log("Made Page");
+
     await page.setViewport({
       width: screenshot.pageWidth,
       height: screenshot.pageHeight,
     });
     console.log("Set viewport");
+
     await page.goto(screenshot.url);
     console.log("Went to ", screenshot.url);
+
     await page.waitUntilVisible(screenshot.waitForSelector, 5000);
+    console.log("Is visible", screenshot.waitForSelector);
+
     await page.evaluate((pageY) => {
       window.scrollBy(0, pageY);
     }, screenshot.scrollY);
-    console.log("Is visible", screenshot.waitForSelector);
-    const image = await page.screenshot({ type: "png" });
+    const image = await page.screenshot({ type: "jpeg", quality: 100 });
     console.log("Made screenshot");
+
+    const filename = `${chosenScreenshot}-${new Date()}`
+
     result = {
       statusCode: 200,
       headers: {
-        "Content-Type": "image/png",
+        "Content-Type": "image/jpeg",
+        'Content-Disposition': `attachment; filename="${filename}"`,
       },
       body: image.toString("base64"),
       isBase64Encoded: true,
     };
   } catch (error) {
-    console.log("ERROR", error);
     return callback(error);
   } finally {
     if (browser !== null) {
       await browser.close();
     }
   }
-
-  console.log("Result", result);
 
   return callback(null, result);
 };
