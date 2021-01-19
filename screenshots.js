582344d28ad87403b19f16085b248d5a86a8d163
Demjan <demjan.vester@gmail.com>
2021-01-19 13:55:45 +0100

    add icuPatients

--- screenshots.js
+++ screenshots.js
@@ -49,4 +49,22 @@ module.exports = {
     pageHeight: 553,
     scrollY: 33,
   },
+  IcuPatients: {
+    url: "https://covid-19.sledilnik.org/embed.html#/chart/IcuPatients",
+    waitForSelector: ".highcharts-root",
+    beforeShot: function() {
+      Highcharts.charts[0].series[0].animationTimeout = 0
+      Highcharts.charts[0].rangeSelector.clickButton(1)
+      document.querySelector('main > .container').style['max-width'] = '100%'
+      document.querySelector('main > .container').style.padding = '0'
+      document.querySelector('.visualization.container.embeded').style['max-width'] = '100%'
+      document.querySelector('.visualization.container.embeded').style.padding = 0
+      document.querySelector('.embeded.col').style.padding = 0
+      document.querySelector('*').style.overflow = 'hidden'
+      Highcharts.charts[0].reflow()
+    },
+    pageWidth: 1061,
+    pageHeight: 553,
+    scrollY: 33,
+  },
 };
